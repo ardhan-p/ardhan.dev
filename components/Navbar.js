169c9835b924c15af5c47be2bc5716714615d6c0
ardhan <ardhan.s.pratama@gmail.com>
2022-12-14 17:19:24 +0700

    Add Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <header>
+      <div class="navbar">
+        <ul class="navbar-list">
+          <li class="nav-left"><a id="website-title">ardhan.dev</a></li>
+          <li class="nav-right"><a href="mailto:ardhan.s.pratama@gmail.com" target="_blank">email</a></li>
+          <li class="nav-right"><a href="#blogs">blog</a></li>
+          <li class="nav-right"><a href="#projects">projects</a></li>
+          <li class="nav-right"><a href="#about">about</a></li>
+        </ul>
+      </div>
+    </header>
+  )
+}
