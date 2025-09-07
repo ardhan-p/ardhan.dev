945248b53cba2361eb1ea8fa493fa7c73db83000
ardhan <ardhan.s.pratama@gmail.com>
2025-09-07 19:30:58 +0700

    feat: update data with placeholder value

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,13 @@ export const personalInfo = {
 	username: "ardhan.dev",
 	title: "Software Engineer",
 	website: "https://ardhan.dev",
+	email: "ardhan.s.pratama@gmail.com",
 	linkedin: "https://www.linkedin.com/in/ardhan-p/",
 	github: "https://github.com/ardhan-p",
 };
 
+export const aboutMeParagraphs = [
+	"hello world 1!",
+	"hello world 2!",
+	"hello world 3!",
+];
