97a2b2ee14c0315d7affe0bc18a62e3b326ac222
ardhan <ardhan.s.pratama@gmail.com>
2025-09-07 19:30:48 +0700

    feat: update app with new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import './App.css'
+import About from './components/About'
 import Header from './components/Header'
+import Section from './components/Section'
 
 function App() {
   return (
     <>
       <Header/>
+      <main>
+        <About/>
+      </main>
     </>
   )
 }
