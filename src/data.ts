1eff0cca1ec6cfc89801ca1e360a5bd72cc9b672
ardhan <ardhan.s.pratama@gmail.com>
2025-09-07 19:10:44 +0700

    feat: add personal info const data

---
+++ src/data.ts
@@ -0,0 +1,9 @@
+export const personalInfo = {
+	name: "Ardhan",
+	username: "ardhan.dev",
+	title: "Software Engineer",
+	website: "https://ardhan.dev",
+	linkedin: "https://www.linkedin.com/in/ardhan-p/",
+	github: "https://github.com/ardhan-p",
+};
+
