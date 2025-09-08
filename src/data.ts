841022a71491e5a411f99b932593f3efedc99e9f
ardhan <ardhan.s.pratama@gmail.com>
2025-09-08 16:34:57 +0700

    feat: update profile intro

--- src/data.ts
+++ src/data.ts
@@ -8,7 +8,13 @@ export const personalInfo = {
 	github: "https://github.com/ardhan-p",
 };
 
-export const aboutMeParagraphs = [
+export const heroParagraphs = [
+	"I'm currently a software engineer working in Samsung Research Indonesia.",
+	"Making a positive difference in people's lives through technology has always been cool to me, and I'm inspired to learn as much as I can to achieve that.",
+	"Feel free to connect with me! (or not, up to you really.)"
+];
+
+export const aboutParagraphs = [
 	"hello world 1!",
 	"hello world 2!",
 	"hello world 3!",
