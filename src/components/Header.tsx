1be76caeeea941d06223ad106d5de3784665645a
ardhan <ardhan.s.pratama@gmail.com>
2025-09-08 22:20:22 +0700

    feat: update header CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ const Header = () => {
   const navigationLinks = ["about", "experience"];
 
   return (
-    <header className="bg-gray-50 bg-opacity-80 backdrop-blur-md fixed top-0 left-0 right-0 z-50">
+    <header className="bg-gray-50 bg-opacity-80 backdrop-blur-xl fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-20">
           <a 
             href="#"
-            className="text-xl hover:text-white hover:bg-gray-700 transition-colors rounded-md p-2"
+            className="text-xl font-semibold hover:text-white hover:bg-gray-700 transition-colors rounded-md p-2"
           >
             {personalInfo.username}
           </a>
