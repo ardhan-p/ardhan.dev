f478eda5e935a90cddd15094f1a8b492187a50b3
ardhan <ardhan.s.pratama@gmail.com>
2025-09-08 22:20:50 +0700

    feat: implement hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,50 +1,72 @@
 import { heroParagraphs, personalInfo } from "../data";
+import { EmailIcon, GithubIcon, LinkedinIcon } from "../icons";
 
 const Hero = () => {
+   return (
+    <section
+      id="hero"
+      className="pt-40 pb-20" 
+    >
+      <div className="container mx-auto px-15">
+        <div className="grid grid-cols-2 gap-12">
+          <div>
+            <h1 className="text-3xl font-bold my-4">
+              Hey, {personalInfo.name} here.
+            </h1>
 
-  return (
-    <section>
-      <div>
-        <div>
-          <h1>
-            Hey, {personalInfo.name} here.
-          </h1>
-        </div>
-        
-        <div>
-          <h2>
-            {personalInfo.title}
-          </h2>
-        </div>
+            {heroParagraphs.map((paragraph, index) => (
+              <p
+                key={index}
+                className="py-2"
+              >
+                {paragraph}
+              </p>
+            ))}
 
-        <div>
-          {heroParagraphs.map((paragraph, index) => (
-            <p 
-              key={index}
+            <div
+              className="flex my-4" 
             >
-              {paragraph}
-            </p>
-          ))}
+              <a 
+                href={personalInfo.github}
+                target="_blank"
+                className="mr-4"
+              >
+                <GithubIcon className="hover:text-white hover:bg-gray-700 transition-colors rounded-md p-1"/>
+              </a>
+
+              <a 
+                href={personalInfo.linkedin}
+                target="_blank"
+                className="mr-4"
+              >
+                <LinkedinIcon className="hover:text-white hover:bg-gray-700 transition-colors rounded-md p-1"/>
+              </a>
+
+              <a 
+                href={personalInfo.email}
+                target="_blank"
+                className="mr-4"
+              >
+                <EmailIcon className="hover:text-white hover:bg-gray-700 transition-colors rounded-md p-1"/>
+              </a>
+            </div>
+
+          </div>
+
+          <div
+            className="flex justify-center" 
+          >
+            <div className="relative h-64 w-64 my-4">
+              <img 
+                className="rounded-md shadow-2xl"
+                src="src/public/ardhan-pfp-1.jpeg"
+              />
+            </div>
+          </div>
+
         </div>
-        <div>
-          <a href={personalInfo.linkedin}>
-            linkedin
-          </a>
 
-          <a href={personalInfo.github}>
-            github
-          </a>
-
-          <a href={personalInfo.email}>
-            email
-          </a>
-        </div>
       </div>
-      
-      <div>
-        profile image
-      </div>
-
     </section>    
   );
 };
