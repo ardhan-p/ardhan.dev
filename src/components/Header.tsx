2ce9ff6a34880e53786d0d7b6a91901ee65bc635
ardhan <ardhan.s.pratama@gmail.com>
2025-09-08 16:26:10 +0700

    feat: update header code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,37 @@
 import { personalInfo } from "../data";
 
 const Header = () => {
-  const navigationLinks = ["about", "experience", "skills", "projects"];
+  const navigationLinks = ["about", "experience"];
 
   return (
-    <header>
-      <div>
-        <a 
-          href="#"
-        >
-          {personalInfo.username}
-        </a>
+    <header className="bg-gray-100 bg-opacity-80 backdrop-blur-md fixed top-0 left-0 right-0 z-50">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between items-center h-20">
+          <a 
+            href="#"
+            className="hover:text-white hover:bg-gray-700 transition-colors rounded-md p-2"
+          >
+            {personalInfo.username}
+          </a>
 
-        <nav>
-          {navigationLinks.map((link) => (
+          <nav className="hidden md:flex items-center space-x-8">
+            {navigationLinks.map((link) => (
+              <a
+                key={link}
+                href={`#${link}`}
+                className="hover:text-white hover:bg-gray-700 transition-colors rounded-md p-2 m-2"
+              >
+                {link}
+              </a>
+            ))}
             <a
-              key={link}
-              href={`#${link}`}
+              href={`mailto:${personalInfo.email}`}
+              className="hover:text-white hover:bg-gray-700 transition-colors rounded-md p-2 m-2"
             >
-              {link}
+              email
             </a>
-          ))}
-        </nav>
-
-        <a
-          href={`mailto:${personalInfo.email}`}
-        >
-          contact
-        </a>
+          </nav>
+        </div>
       </div>
     </header>
   );
