278e58c58f6846dea6594b0a0a3b399eddc8da3c
ardhan <ardhan.s.pratama@gmail.com>
2025-11-16 21:56:16 +0700

    chore: update personal information

--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,12 @@
+import type { Experience } from "./types";
+
 export const personalInfo = {
 	name: "Ardhan",
+	fullName: "Ardhan Satya Pratama",
 	username: "ardhan.dev",
 	title: "Software Engineer",
 	website: "https://ardhan.dev",
-	email: "ardhan.s.pratama@gmail.com",
+	email: "mailto:ardhan.s.pratama@gmail.com",
 	linkedin: "https://www.linkedin.com/in/ardhan-p/",
 	github: "https://github.com/ardhan-p",
 };
@@ -15,7 +18,28 @@ export const heroParagraphs = [
 ];
 
 export const aboutParagraphs = [
-	"hello world 1!",
 	"I'm passionate with transforming abstract ideas into awesome technical projects, whether it is through techincal leadership, or through clean and maintainable software engineering.",
-	"hello world 3!",
+];
+
+export const experienceData: Experience[] = [
+	{
+		title: "Software Engineer - Technical Lead",
+		company: "Samsung Research Indonesia",
+		description: [
+			"Led a team of software engineers to develop and maintain server-side applications for Samsung's Smart TV platform at a global scale.",
+		],
+		skill: ["JavaScript", "Tizen SDK", "UI/UX Design", "Performance Optimization"],
+		duration: "Sep 2024 - Present",
+		link: "https://www.samsung.com/id/srin/",
+	},
+	{
+		title: "Software Engineer",
+		company: "Samsung Research Indonesia",
+		description: [
+			"Repsonsible for developing and maintaining server-side applications for Samsung's Smart TV platform.",
+		],
+		skill: ["JavaScript", "Tizen SDK", "UI/UX Design", "Performance Optimization"],
+		duration: "Apr 2023 - Sep 2024",
+		link: "https://www.samsung.com/id/srin/",
+	},
 ];
