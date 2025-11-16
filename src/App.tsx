0b370be44c1b2d8d29c29016d2d3ee3d13012740
ardhan <ardhan.s.pratama@gmail.com>
2025-11-16 21:56:28 +0700

    feat: add Experience in app tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Header from './components/Header'
 import Hero from './components/Hero'
-import About from './components/About'
 import Experience from './components/Experience'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -9,7 +9,9 @@ function App() {
       <Header/>
       <main>
         <Hero/>
+        <Experience/>
       </main>
+      <Footer/>
     </>
   )
 }
